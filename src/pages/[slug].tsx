3b5e1e7bbf22d5f5fe32147aef04a5182587e12e
i am Chuppyy <62337417+chuppyy@users.noreply.github.com>
2024-05-09 22:48:32 +0700

    Update [slug].tsx

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -157,13 +157,13 @@ export default function Page(data: any) {
         <meta property="og:image" content={article.avatarLink} />
         <meta property="og:title" content={article.name+"-"+article.userCode} />
       </Head>
-      <Script id="gg-1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-Y23MD9WKC2`} />
+      <Script id="gg-1" strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=G-5JYFPYR470`} />
       <Script id="gg-2" strategy="lazyOnload">
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', 'G-Y23MD9WKC2');
+          gtag('config', 'G-5JYFPYR470');
         `}
       </Script>
       <Script
