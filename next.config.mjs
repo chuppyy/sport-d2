b3ec131aeed8f84c7ce2bf67465aafdac594057c
Thiện Tạ Khánh <takhanhthient3@gmail.com>
2024-06-03 14:10:04 +0700

    kk

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["apisport.sportsandtravelonline.com","api.sportsandtravelonline.com"],
+    domains: ["apisport.vbonews.com","api.vbonews.com"],
   },
 };
 
